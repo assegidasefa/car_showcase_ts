d99ac4b94ba9915689a2291575e263d021f66c08
assegidasefa <assegidassefa7@gmail.com>
2023-10-09 10:35:25 +0300

    initializing project

---
+++ components/Hero.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-3 padding-x">
+        <h1 className="hero__title">
+          Find , Book or rent a car -quickly and easily
+        </h1>
+        <p className="hero__subtitle">
+          streamline your car rental experiance with out effortless booking
+          process.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
